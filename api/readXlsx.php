13907bd38f46e908ba6b26b4022e9351ede64ef3
thomas-rooty <essainono1@gmail.com>
2022-03-18 14:48:38 +0100

    for vercel

--- api/readXlsx.php
+++ api/readXlsx.php
@@ -9,40 +9,52 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 if (isset($_FILES['file']['name'])) {
     // file name
     $filename = $_FILES['file']['name'];
-    $file = $_FILES['file']['tmp_name'];
+
+    // Location
+    $location = 'uploads/' . $filename;
 
     // Check if file is a xlsx file
-    //$fileType = pathinfo($filename, PATHINFO_EXTENSION);
+    //$fileType = pathinfo($location, PATHINFO_EXTENSION);
     $fileType = 'xlsx';
     $response = 0;
 
     if ($fileType === 'xlsx') {
-        // Get this temp file
-        $spreadsheet = IOFactory::load($file);
-        $sheet = $spreadsheet->getSheet(2);
+        // Upload file
+        if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+            // Get this file
+            /*
+            $spreadsheet = IOFactory::load($location);
+            $sheet = $spreadsheet->getSheet(2);
 
-        // Create contractPrices array and get cells from cellsToRead array on sheet 2
-        $contractPrices = array(
-            "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
-            "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
-            "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
-            "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),    //[2]
-            "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '') //[4]
-        );
-        // customerAddress_ID
-        $siteID = $_POST['customerAddress_ID'];
+            // Create contractPrices array and get cells from cellsToRead array on sheet 2
+            $contractPrices = array(
+                "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
+                "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
+                "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
+                "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),    //[2]
+                "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '') //[4]
+            );
+            // customerAddress_ID
+            $siteID = $_POST['customerAddress_ID'];
 
-        // Return response with all the prix in json format
-        $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
+            // Return response with all the prix in json format
+            $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
 
-        // Store the contractPrices into the db
-        storeContractPrices($siteID, $contractPrices);
-        $response = "coucou";
+            // Store the contractPrices into the db
+            storeContractPrices($siteID, $contractPrices);
+            */
+            $response = "coucou";
+        } else {
+            $response = 'File upload failed, please try again.';
+        }
     } else {
         // Not a xlsx file
         $response = 'Please upload a xlsx file';
     }
 
     echo $response;
+
+    // Delete the file and exit the script
+    unlink($location);
     exit;
 }
