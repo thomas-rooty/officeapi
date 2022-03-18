119283f444844848483dc8938b8ac5f6fc84b96f
thomas-rooty <essainono1@gmail.com>
2022-03-18 11:12:34 +0100

    fixed dir

--- api/readXlsx.php
+++ api/readXlsx.php
@@ -10,28 +10,35 @@ if (isset($_FILES['file']['name'])) {
     // file name
     $filename = $_FILES['file']['name'];
 
+    // Check if file is a xlsx file
+    $fileType = pathinfo($filename, PATHINFO_EXTENSION);
     $response = 0;
 
-    // Get this temp file
-    $spreadsheet = IOFactory::load($filename);
-    $sheet = $spreadsheet->getSheet(2);
+    if ($fileType === 'xlsx') {
+        // Get this temp file
+        $spreadsheet = IOFactory::load($filename);
+        $sheet = $spreadsheet->getSheet(2);
 
-    // Create contractPrices array and get cells from cellsToRead array on sheet 2
-    $contractPrices = array(
-        "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
-        "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
-        "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
-        "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),    //[2]
-        "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '') //[4]
-    );
-    // customerAddress_ID
-    $siteID = $_POST['customerAddress_ID'];
+        // Create contractPrices array and get cells from cellsToRead array on sheet 2
+        $contractPrices = array(
+            "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
+            "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
+            "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
+            "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),    //[2]
+            "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '') //[4]
+        );
+        // customerAddress_ID
+        $siteID = $_POST['customerAddress_ID'];
 
-    // Return response with all the prix in json format
-    $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
+        // Return response with all the prix in json format
+        $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
 
-    // Store the contractPrices into the db
-    storeContractPrices($siteID, $contractPrices);
+        // Store the contractPrices into the db
+        storeContractPrices($siteID, $contractPrices);
+    } else {
+        // Not a xlsx file
+        $response = 'Please upload a xlsx file';
+    }
 
     echo $response;
 
