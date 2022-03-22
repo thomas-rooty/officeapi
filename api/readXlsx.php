4d647bec202ff02f7f1196b6d4e6d9b325c6de7e
thomas-rooty <essainono1@gmail.com>
2022-03-22 09:56:42 +0100

    changed api to work in local

--- api/readXlsx.php
+++ api/readXlsx.php
@@ -6,55 +6,51 @@ require 'functions.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
-
 if (isset($_FILES['file']['name'])) {
-    // file name
-    $filename = $_FILES['file']['name'];
-    $tmp = $_FILES['file']['tmp_name'];
+    // Check if file has been uploaded
+    if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+        // file name
+        $filename = $_FILES['file']['name'];
+        $tmp = $_FILES['file']['tmp_name'];
 
-    // Location
-    $location = 'uploads/' . $filename;
+        // Location
+        $location = 'uploads/' . $filename;
 
-    // Check if file is a xlsx file
-    //$fileType = pathinfo($location, PATHINFO_EXTENSION);
-    $fileType = 'xlsx';
-    $response = 0;
+        // Check if file is a xlsx file
+        //$fileType = pathinfo($location, PATHINFO_EXTENSION);
+        $fileType = 'xlsx';
+        $response = 0;
 
-    if ($fileType === 'xlsx') {
-        // Get this file
-        /*
-        $spreadsheet = IOFactory::load($tmp);
-        $sheet = $spreadsheet->getSheet(2);
+        if ($fileType === 'xlsx') {
+            // Get this file
+            $spreadsheet = IOFactory::load($tmp);
+            $sheet = $spreadsheet->getSheet(2);
 
-        // Create contractPrices array and get cells from cellsToRead array on sheet 2
-        $contractPrices = array(
-            "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
-            "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
-            "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
-            "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),    //[2]
-            "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '') //[4]
-        );
-        // customerAddress_ID
-        $siteID = $_POST['customerAddress_ID'];
+            // Create contractPrices array and get cells from cellsToRead array on sheet 2
+            $contractPrices = array(
+                "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
+                "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
+                "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
+                "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),    //[2]
+                "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '') //[4]
+            );
+            // customerAddress_ID
+            $siteID = $_POST['customerAddress_ID'];
 
-        // Return response with all the prix in json format
-        $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
+            // Return response with all the prix in json format
+            $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
 
-        // Store the contractPrices into the db
-        storeContractPrices($siteID, $contractPrices);
-        */
-        $spreadsheet = IOFactory::load($tmp);
-        $sheet = $spreadsheet->getSheet(2);
-        $response = $sheet->getCell('D238')->getCalculatedValue();
+            // Store the contractPrices into the db
+            storeContractPrices($siteID, $contractPrices);
+            unlink($tmp);
+        } else {
+            $response = "File hasn't been uploaded";
+        }
     } else {
         // Not a xlsx file
         $response = 'Please upload a xlsx file';
     }
 
     echo $response;
-
-    // Delete the file and exit the script
-    //unlink($location);
     exit;
 }
