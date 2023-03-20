67bf4ae4ee7c19467fe7320e69f81f2e7323d4c0
thomas.caron <tc.caron91@gmail.com>
2023-03-20 10:53:59 +0100

    Improved readability + reduced nesting

--- api/readXlsx.php
+++ api/readXlsx.php
@@ -6,52 +6,55 @@ header('Access-Control-Allow-Origin: *');
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-if (isset($_FILES['file']['name'])) {
-  // Check if file has been uploaded
-  if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-    // file name
-    $filename = $_FILES['file']['name'];
-    $tmp = $_FILES['file']['tmp_name'];
-    
-    // Location
-    $location = 'uploads/' . $filename;
-    
-    // Check if file is a xlsx file
-    //$fileType = pathinfo($location, PATHINFO_EXTENSION);
-    $fileType = 'xlsx';
-    $response = 0;
-    
-    if ($fileType === 'xlsx') {
-      // Get this file
-      $spreadsheet = IOFactory::load($tmp);
-      $sheet = $spreadsheet->getSheet(2);
-      
-      // Create contractPrices array and get cells from cellsToRead array on sheet 2
-      $contractPrices = array(
-        "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),  //[0]
-        "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),  //[1]
-        "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),    //[2]
-        "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),//[3]
-        "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '')//[4]
-      );
-      // customerAddress_ID
-      $siteID = $_POST['customerAddress_ID'];
-      
-      // Return response with all the prix in json format
-      try {
-        $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
-      } catch (JsonException $e) {
-        $response = "Error: " . $e->getMessage();
-      }
-      unlink($tmp);
-    } else {
-      $response = "File hasn't been uploaded";
-    }
-  } else {
-    // Not a xlsx file
-    $response = 'Please upload a xlsx file';
-  }
-  
+// Check if a file has been uploaded
+if (!isset($_FILES['file']['name'])) {
+  // File not uploaded
+  $response = "File hasn't been uploaded";
   echo $response;
   exit;
 }
+
+// Retrieve the file
+$tmp = $_FILES['file']['tmp_name'];
+$filename = $_FILES['file']['name'];
+$location = 'uploads/' . $filename;
+
+// Check if file is a xlsx file
+$fileType = pathinfo($location, PATHINFO_EXTENSION);
+$response = 0;
+
+if ($fileType !== 'xlsx') {
+  // File type is not valid
+  $response = "Please upload a xlsx file";
+  echo $response;
+  exit;
+}
+
+// Load the spreadsheet and get the second sheet
+$spreadsheet = IOFactory::load($tmp);
+$sheet = $spreadsheet->getSheet(2);
+
+// Create the `contractPrices` array and get cells from `cellsToRead` array on sheet 2
+$contractPrices = [
+  "contract_Ivoire" => number_format($sheet->getCell('D238')->getCalculatedValue(), 1, '.', ''),
+  "contract_Silver" => number_format($sheet->getCell('D239')->getCalculatedValue(), 1, '.', ''),
+  "contract_Gold" => number_format($sheet->getCell('D241')->getCalculatedValue(), 1, '.', ''),
+  "contract_GoldPlus" => number_format($sheet->getCell('D243')->getCalculatedValue(), 1, '.', ''),
+  "contract_Platinium" => number_format($sheet->getCell('D245')->getCalculatedValue(), 1, '.', '')
+];
+
+// Get the `customerAddress_ID`
+$siteID = $_POST['customerAddress_ID'];
+
+// Return response with all the prices in JSON format
+try {
+  $response = json_encode($contractPrices, JSON_THROW_ON_ERROR);
+} catch (JsonException $e) {
+  $response = "Error: " . $e->getMessage();
+}
+
+// Remove the uploaded file
+unlink($tmp);
+
+echo $response;
+exit;
